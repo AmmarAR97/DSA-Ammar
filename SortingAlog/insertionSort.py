161099510f4d35eca6294a96a1b1e709fb6edd06
AmmarAR97 <ammar.abdur.1@gmail.com>
2023-10-26 08:28:43 +0530

    added comments

--- SortingAlog/insertionSort.py
+++ SortingAlog/insertionSort.py
@@ -1,13 +1,20 @@
+# Define a function named insertionSort that takes an unsorted list as input.
 def insertionSort(unsortedList):
+	# Iterate through the list starting from the second element (index 1).
 	for i in range(1, len(unsortedList)):
+		# Store the current element in the 'key' variable.
 		key = unsortedList[i]
+		# Initialize 'j' to the index before the current element.
 		j = i - 1
+		# Compare 'key' with elements before it and shift them right until
+		# the correct position for 'key' is found.
 		while j >= 0 and key < unsortedList[j]:
-			unsortedList[j+1] = unsortedList[j]
-			j -= 1
-			print(unsortedList)
+			unsortedList[j+1] = unsortedList[j]  # Shift the element to the right.
+			j -= 1  # Move to the previous element.
+		# Place 'key' in its correct position in the sorted portion of the list.
 		unsortedList[j+1] = key
-		print("s",unsortedList)
+	# Return the sorted list.
 	return unsortedList
 
-print(insertionSort([5,2,6,12,1,8,3,9]))
+# Call the insertionSort function with an unsorted list as an argument and print the result.
+print(insertionSort([5, 2, 6, 12, 1, 8, 3, 9]))
