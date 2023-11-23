edf865d54fd82544024ea89440f3e309178f316f
AmmarAR97 <ammar.abdur.1@gmail.com>
2023-11-23 16:16:18 +0530

    completed graph ds

--- Graph/graph.py
+++ Graph/graph.py
@@ -45,6 +45,42 @@ class Graph:
             return True
         return False
 
+    def bfs(self, vertex):
+        # Set to keep track of visited vertices
+        visited = set()
+        
+        # Mark the starting vertex as visited and initialize the queue
+        visited.add(vertex)
+        queue = [vertex]
+
+        # Continue BFS until the queue is empty
+        while queue:
+            # Get the current vertex from the front of the queue
+            current_vertex = queue.pop(0)
+            
+            print(current_vertex)
+
+            # Explore adjacent vertices
+            for adjacent_vertex in self.adjacency_list[current_vertex]:
+                # Visit unvisited adjacent vertices and enqueue them
+                if adjacent_vertex not in visited:
+                    visited.add(adjacent_vertex)
+                    queue.append(adjacent_vertex)
+
+    def dfs(self, vertex):
+        visited = set()
+        stack = [vertex]
+        while stack:
+            current_vertex = stack.pop()
+            if current_vertex not in visited:
+                print(current_vertex)
+                visited.add(current_vertex)
+            for adjacent_vertex in self.adjacency_list[current_vertex]:
+                if adjacent_vertex not in visited:
+                    stack.append(adjacent_vertex)
+
+
+
 # Create a Graph object
 my_graph = Graph()
 
@@ -53,20 +89,27 @@ my_graph.add_vertex("A")
 my_graph.add_vertex("B")
 my_graph.add_vertex("C")
 my_graph.add_vertex("D")
+my_graph.add_vertex("E")
 
 # Add edges to connect the vertices
 my_graph.add_edge("A", "B")
 my_graph.add_edge("A", "C")
-my_graph.add_edge("A", "D")
-my_graph.add_edge("B", "C")
+my_graph.add_edge("B", "E")
 my_graph.add_edge("C", "D")
+my_graph.add_edge("D", "E")
 
-# Print the graph's adjacency list
-my_graph.print_graph()
+# # Print the graph's adjacency list
+# my_graph.print_graph()
 
-# Remove vertex "A" and its connections from the graph
-my_graph.remove_vertex("A")
+# # Remove vertex "A" and its connections from the graph
+# my_graph.remove_vertex("A")
 
-# Print the graph again after removing "A"
-print("After remove..")
-my_graph.print_graph()
+# # Print the graph again after removing "A"
+# print("After remove..")
+# my_graph.print_graph()
+
+# print using bfs
+# my_graph.bfs("A")
+
+# print using dfs
+my_graph.dfs("A")
